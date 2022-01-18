4c3047af08fb4f1edc61573faf76deaa7c67a97c
Claudio F. Viera M <cl_fran_vm@hotmail.com>
2022-01-18 16:39:11 -0300

    fix zone

--- modules/kubectl-wrapper/main.tf
+++ modules/kubectl-wrapper/main.tf
@@ -30,8 +30,8 @@ module "gcloud_kubectl" {
   use_tf_google_credentials_env_var = var.use_tf_google_credentials_env_var
 
   create_cmd_entrypoint  = "${path.module}/scripts/kubectl_wrapper.sh"
-  create_cmd_body        = var.impersonate_service_account == "" ? "${local.base_cmd} ${var.kubectl_create_command}" : "${local.base_cmd} true ${var.impersonate_service_account} ${var.kubectl_create_command}"
+  create_cmd_body        = var.impersonate_service_account == "" ? "${local.base_cmd} ${var.kubectl_create_command}" : "'${local.base_cmd}' true ${var.impersonate_service_account} ${var.kubectl_create_command}"
   create_cmd_triggers    = var.create_cmd_triggers
   destroy_cmd_entrypoint = "${path.module}/scripts/kubectl_wrapper.sh"
-  destroy_cmd_body       = var.impersonate_service_account == "" ? "${local.base_cmd} ${var.kubectl_destroy_command}" : "${local.base_cmd} true ${var.impersonate_service_account} ${var.kubectl_destroy_command}"
+  destroy_cmd_body       = var.impersonate_service_account == "" ? "${local.base_cmd} ${var.kubectl_destroy_command}" : "'${local.base_cmd}' true ${var.impersonate_service_account} ${var.kubectl_destroy_command}"
 }
